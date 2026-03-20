905f433c287f782a67ea499b214c1893babb7c0b
ray5497-k <jonghyeon0804@gmail.com>
2026-03-20 19:52:03 +0900

    test

---
+++ ray5497-k/1_2739 복사본.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, N ;
+
+    scanf("%d", &N);
+
+    for (i = 1 ; i < 10 ; i++)
+    {
+        printf("%d * %d = %d\n", N,i, N * i);
+    }
+
+    return 0;
+}
